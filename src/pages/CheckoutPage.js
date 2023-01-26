95f2df077fa3931dfb20f0d648f4c764394797e5
Eduardo Souza <edcampossouza@gmail.com>
2023-01-26 10:23:28 -0300

    feat CheckoutPage: estilo

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,10 +1,111 @@
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import Header from "../components/Header";
+import AppContext from "../context/AppContext";
+import {
+  Title,
+  ButtonStyle,
+  ButtonsContainer,
+  PageContainer,
+} from "../style/sharedStyles";
+import { formataReais } from "../util/util";
 
 export default function CheckoutPage() {
+  const { cartItems } = useContext(AppContext);
+  const [valor, setValor] = useState(0);
+  const [items, setItems] = useState(0);
+
+  const [numCartao, setNumCartao] = useState("");
+
+  useEffect(() => {
+    setValor(cartItems.reduce((p, c) => p + c.price, 0));
+    setItems(cartItems.reduce((p, c) => p + c.quantity, 0));
+  }, [cartItems]);
+
   return (
     <>
       <Header />
-      Checkout
+      <PageContainer>
+        <Title>Checkout</Title>
+        <StyledSection>
+          <h2>Totais</h2>
+          <p> Valor total: {formataReais(valor)}</p>
+          <p>Itens no carrinho: {items}</p>
+        </StyledSection>
+        <StyledForm>
+          <StyledSection>
+            <h2>Dados do pagamento</h2>
+            <InputRow>
+              <label>Numero do Cartao:</label>
+              <input value={numCartao} placeholder="000-000" required />
+            </InputRow>
+          </StyledSection>
+          <StyledSection>
+            <h2>Dados da Entrega</h2>
+            <InputRow>
+              <label>CEP:</label>
+              <input value={numCartao} placeholder="00000-000" required />
+            </InputRow>
+            <InputRow>
+              <label>Logradouro:</label>
+              <input
+                class="logradouro"
+                value={numCartao}
+                placeholder="Rua x, num 0"
+                required
+              />
+            </InputRow>
+            <InputRow>
+              <span></span>
+              <ButtonStyle>Confirmar Pedido</ButtonStyle>
+            </InputRow>
+          </StyledSection>
+        </StyledForm>
+
+        <ButtonsContainer>
+          <ButtonStyle>
+            <Link to="/cart">Ver Carrinho</Link>
+          </ButtonStyle>
+          <ButtonStyle>
+            <Link to="/">Continuar comprando</Link>
+          </ButtonStyle>
+        </ButtonsContainer>
+      </PageContainer>
     </>
   );
 }
+
+const StyledSection = styled.section`
+  margin-bottom: 20px;
+  width: 715px;
+  h2 {
+    font-size: 20px;
+  }
+  input {
+    border: 1px solid black;
+    height: 25px;
+    border-radius: 5px;
+    margin-left: 5px;
+    ::placeholder {
+      text-align: center;
+    }
+  }
+  input.logradouro {
+    width: 250px;
+  }
+  @media screen and (max-width: 715px) {
+    width: 100%;
+  }
+`;
+const StyledForm = styled.form`
+  width: 715px;
+  @media screen and (max-width: 715px) {
+    width: 100%;
+  }
+`;
+const InputRow = styled.div`
+  margin-bottom: 5px;
+  display: flex;
+  justify-content: space-between;
+`;
---
+++ src/style/sharedStyles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 36px;
+`;
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 0px 10px;
+`;
+
+export const ButtonStyle = styled.button`
+  background-color: #6383a7;
+  font-size: 16px;
+  height: 40px;
+  border-radius: 5px;
+  color: white;
+  margin-bottom: 5px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  a {
+    color: white;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  button {
+    border: none;
+    width: 200px;
+    border-radius: 5px;
+    margin: 10px 15px;
+  }
+  @media (max-width: 715px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
